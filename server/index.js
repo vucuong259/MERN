6a6cd54026af05ad3988d7ea2976ee01ca033eb3
cuongvm <cuongvm@newwave.vn>
2021-04-27 18:02:31 +0700

    connect db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://cuongvm:1234@mern.ougxi.mongodb.net/MERN?retryWrites=true&w=majority`, {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
+        console.log('MongDb connected')
+    } catch (error) {   
+        console.log(error);
+        process.exit(1);
+    }
+}
 
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello'));
 
